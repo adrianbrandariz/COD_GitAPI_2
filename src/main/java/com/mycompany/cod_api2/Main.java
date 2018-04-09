689883c753577505cd61c5cd79a4475ff92adf2f
Hansen@DESKTOP-P1ST9AA <hansennygard@gmail.com>
2018-04-09 12:59:50 +0200

    Inicio de proyecto

---
+++ src/main/java/com/mycompany/cod_api2/Main.java
@@ -0,0 +1,13 @@
+package com.mycompany.cod_api2;
+
+/**
+ *
+ * @author abrandarizdominguez
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // Inicio de proyecto
+    }
+    
+}
