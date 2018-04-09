ceb666309baa79aa4d9de8b0b1a820d5c557a4f4
Hansen@DESKTOP-P1ST9AA <hansennygard@gmail.com>
2018-04-09 13:18:23 +0200

    Metodo "cloneRepository" añadido

--- src/main/java/com/mycompany/cod_api2/MetodosGitHub.java
+++ src/main/java/com/mycompany/cod_api2/MetodosGitHub.java
@@ -1,9 +1,26 @@
 package com.mycompany.cod_api2;
 
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
 /**
  *
  * @author abrandarizdominguez
  */
 public class MetodosGitHub {
     
+    public void cloneRepository(){
+        try {
+            Git.cloneRepository()
+                    .setURI("https://github.com/eclipse/jgit.git")
+                    .setDirectory(new File("C:\\Users\\Hansen\\Documents\\NetBeansProjects\\COD_PRUEBA_API2"))
+                    .call();
+        } catch (GitAPIException ex) {
+            Logger.getLogger(MetodosGitHub.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error: "+ex);
+        }
+    }
 }
