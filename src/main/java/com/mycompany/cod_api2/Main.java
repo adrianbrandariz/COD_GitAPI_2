835634edab1b9c69afc24d6c684f58ac821dc8e6
Hansen@DESKTOP-P1ST9AA <hansennygard@gmail.com>
2018-06-01 09:26:05 +0200

    static en main

--- src/main/java/com/mycompany/cod_api2/Main.java
+++ src/main/java/com/mycompany/cod_api2/Main.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
  */
 public class Main {
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
 
         MetodosGitHub met = new MetodosGitHub();
 
